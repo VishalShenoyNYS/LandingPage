c0ab764ef81bd15b1d6405dd750f81b5d0a120e1
VishalShenoy2002 <vishal.bshenoy@gmail.com>
2024-05-18 15:26:45 +0530

    added new routes

--- app.py
+++ app.py
@@ -13,5 +13,15 @@ def index():
 def about():
     if request.method == "GET":
         return render_template("about.html",title="New You Solutions | About",page_hero_title="",page_hero_video="static/videos/nys-home-video.mp4")  
+
+@app.route('/services',methods=["GET"])
+def services():
+    if request.method == "GET":
+        return render_template("services.html",title="New You Solutions | Services",page_hero_title="",page_hero_video="static/videos/nys-home-video.mp4")  
+
+@app.route('/courses',methods=["GET"])
+def courses():
+    if request.method == "GET":
+        return render_template("courses.html",title="New You Solutions | Courses",page_hero_title="",page_hero_video="static/videos/nys-home-video.mp4")  
 if __name__ == "__main__":
     app.run(debug=True)
