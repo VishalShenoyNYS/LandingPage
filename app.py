88a51dc54a03074a120f1a863ec026cb3a9b8efc
VishalShenoy2002 <vishal.bshenoy@gmail.com>
2024-05-24 22:28:48 +0530

    added routes for connect, stdy abroad etc

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "GET":
-        return render_template("index.html",title="New You Solutions",page_hero_title="",page_hero_video="static/videos/nys-home-video.mp4")
+        return render_template("index.html",title="New You Solutions",page_hero_title="New You Solutions",page_hero_video="static/videos/nys-home-video.mp4")
     
 
 @app.route('/about',methods=["GET"])
@@ -17,11 +17,23 @@ def about():
 @app.route('/services',methods=["GET"])
 def services():
     if request.method == "GET":
-        return render_template("services.html",title="New You Solutions | Services",page_hero_title="",page_hero_video="static/videos/nys-home-video.mp4")  
+        return render_template("services.html",title="New You Solutions | Services")  
 
 @app.route('/courses',methods=["GET"])
 def courses():
     if request.method == "GET":
-        return render_template("courses.html",title="New You Solutions | Courses",page_hero_title="",page_hero_video="static/videos/nys-home-video.mp4")  
+        return render_template("courses.html",title="New You Solutions | Courses")  
+
+@app.route("/connect",methods=["GET","POST"])
+def connect_page():
+    if request.method == "GET":
+        return render_template("connect.html",title="New You Solutions | Connect")
+    
+@app.route("/study-abroad",methods=["GET","POST"])
+def study_abroad_page():
+    if request.method == "GET":
+        return render_template("study_abroad.html",title="New You Solutions | Study Abroad")
+            
+
 if __name__ == "__main__":
     app.run(debug=True)
